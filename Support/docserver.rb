dfae6856f23f51f9ae24e703adb9efb222f7875e
Joachim Mårtensson <joachimm@c-68e2e055.129-2-64736c10.cust.bredbandsbolaget.se>
2010-06-11 23:23:06 +0200

    added completion for Cocoa Classes, functions and constants

--- Support/docserver.rb
+++ Support/docserver.rb
@@ -31,54 +31,87 @@ class Simple < WEBrick::HTTPServlet::AbstractServlet
        doc = generateOBJCDocumentation(query['method'], "div", 1)
       elsif query['constant']
         doc = generateOBJCDocumentation(query['constant'], "div", 0)
-      elsif query['query']
+      elsif query['function']
         doc = generateOBJCDocumentation(query['function'], "div", 0)
+      elsif query['class']
+        doc = generateCocoaClassDocumentation(query['class'])
       end
       content = {"documentation"=> doc} unless doc.nil?    
     end
 
     return 200, "text/plain", content.to_plist
   end
-
+  
+  def generateCocoaClassDocumentation(symbol)
+    url, anchor = run_command(symbol)
+    return nil if url.nil?
+    
+    str = open(url, "r").read
+    
+    searchTerm = "<a name=\"#{anchor}\""
+    startIndex = str.index(searchTerm)
+    return str if startIndex.nil?
+    
+    searchTerm = "<div id=\"Tasks_section\""
+    endIndex = str.index(searchTerm)
+    return nil if endIndex.nil?
+    
+    return str[startIndex...endIndex]
+    
+  end
+  
   def generateOBJCDocumentation( symbol, tag, count)
-       begin
-         docset_cmd = "/Developer/usr/bin/docsetutil search -skip-text -query "
-         sets =  [
-           "/Developer/Documentation/DocSets/com.apple.adc.documentation.AppleSnowLeopard.CoreReference.docset",
-           "/Developer/Documentation/DocSets/com.apple.ADC_Reference_Library.CoreReference.docset",
-         ]
+    begin
+      url, anchor = run_command(symbol)
+      return nil if url.nil?
+      
+      str = open(url, "r").read
+    
+      searchTerm = "<a name=\"#{anchor}\""
+      startIndex = str.index(searchTerm)
+      return str if startIndex.nil?
+      #return str[startIndex.. startIndex + 200]
+      # endIndex = str.index("<a name=\"//apple_ref/occ/", startIndex + searchTerm.length)
+      #endIndex = find_end_tag(tag ,str, startIndex, count)
+      endIndex = find_declared_in(str, startIndex)
+      return nil if endIndex.nil?
+      return str[startIndex...endIndex]
+      
+    rescue Exception => e
+      return "error when generating documentation>" + selection.inspect + e.message + e.backtrace.inspect + symbol + ">>>"+object + url
+    end
+   end
+   
+   def run_command(symbol)
+     docset_cmd = "/Developer/usr/bin/docsetutil search -skip-text -query "
+     sets =  [
+       "/Developer/Documentation/DocSets/com.apple.adc.documentation.AppleSnowLeopard.CoreReference.docset",
+       "/Developer/Documentation/DocSets/com.apple.ADC_Reference_Library.CoreReference.docset",
+     ]
 
-         docset = sets.find do |candidate| 
-           FileTest.exist?(candidate)
-         end
-         
-         return nil if docset.nil?
+     docset = sets.find do |candidate| 
+       FileTest.exist?(candidate)
+     end
+     
+     return nil if docset.nil?
 
-         cmd = docset_cmd + symbol + ' ' + docset
-         result = `#{cmd} 2>&1`
+     cmd = docset_cmd + symbol + ' ' + docset
+     result = `#{cmd} 2>&1`
 
-         status = $?
-         return result if status.exitstatus != 0
+     status = $?
+     return result if status.exitstatus != 0
 
-         firstLine = result.split("\n")[0]
-         urlPart = firstLine.split[1]
-         path, anchor = urlPart.split("\#")
+     firstLine = result.split("\n")[0]
+     urlPart = firstLine.split[1]
+     path, anchor = urlPart.split("\#")
 
-         url = docset + "/Contents/Resources/Documents/" + path
-         str = open(url, "r").read
-
-         searchTerm = "<a name=\"#{anchor}\""
-         startIndex = str.index(searchTerm)
-         return str if startIndex.nil?
-         #return str[startIndex.. startIndex + 200]
-         # endIndex = str.index("<a name=\"//apple_ref/occ/", startIndex + searchTerm.length)
-         endIndex = find_end_tag(tag ,str, startIndex, count)  
-         return nil if endIndex.nil?
-         return str[startIndex...endIndex]
-         
-       rescue Exception => e
-         return "error when generating documentation>" + selection.inspect + e.message + e.backtrace.inspect + symbol + ">>>"+object + url
-       end
+     url = docset + "/Contents/Resources/Documents/" + path
+     return url, anchor
+   end
+   
+   def find_declared_in(str, start)
+     ix = str.index("<code class=\"HeaderFile\">", start)
+     str.index("</code></div>",ix) unless ix.nil?
    end
    
    def find_end_tag(tag, string, start, count=0) 
